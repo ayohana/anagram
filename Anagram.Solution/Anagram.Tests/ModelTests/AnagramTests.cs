f621d6f90ae683c89650ffbdbdb97ed1fe78d306
Adela Darmansyah <adela.yohana@gmail.com>
2020-03-03 11:30:50 -0800

    Add second spec

--- Anagram.Solution/Anagram.Tests/ModelTests/AnagramTests.cs
+++ Anagram.Solution/Anagram.Tests/ModelTests/AnagramTests.cs
@@ -6,11 +6,22 @@ namespace Anagram.Tests
   [TestClass]
   public class WordsTests
   {
+
     [TestMethod]
     public void WordsConstructor_CreatesInstanceOfWords_Words()
-    {
+    { 
       Words testWords = new Words();
       Assert.AreEqual(typeof(Words), testWords.GetType());
+    }
+
+    [TestMethod]
+    public void GetFirstWord_ReturnsFirstWord_String()
+    {
+      string firstTestWord = "bread"; 
+      Words testWords = new Words();
+      testWords.AddWord(firstTestWord);
+
+      Assert.AreEqual(firstTestWord, testWords.ListOfWords[0]);
     }
   }
 }
