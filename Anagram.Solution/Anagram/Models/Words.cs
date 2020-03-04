d91213a8c27f0b6a1d0c2e45366ade4319581f0a
Adela Darmansyah <adela.yohana@gmail.com>
2020-03-03 16:13:08 -0800

    Add methods to check for spaces without using regex and to check for anagrams using string comparisons

--- Anagram.Solution/Anagram/Models/Words.cs
+++ Anagram.Solution/Anagram/Models/Words.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Anagram.Models
 {
   public class Words
@@ -9,6 +11,7 @@ namespace Anagram.Models
       if (CheckForSpaces(input))
       {
         wordsArr = input.Split(" ");
+        Array.Sort(wordsArr);
       }
       else
       {
@@ -19,6 +22,35 @@ namespace Anagram.Models
     public bool CheckForSpaces(string input)
     {
       return input.Contains(" ");
+    }
+
+    public void CheckForAnagrams()
+    {
+      int i = 1;
+      foreach (string word in wordsArr)
+      {
+        char[] word1 = word.ToCharArray();
+        char[] word2 = wordsArr[i].ToCharArray();
+        Array.Sort(word1);
+        Array.Sort(word2);
+        string joined1 = string.Join("", word1);
+        string joined2 = string.Join("", word2);
+        
+        // Compare Two Strings Case-Insensitively
+        if (string.Equals(joined1, joined2, StringComparison.OrdinalIgnoreCase))
+        {
+          Console.WriteLine($"{word} and {wordsArr[i]} are anagrams.");
+        }
+
+        if (i == wordsArr.Length-1)
+        {
+          i = 0;
+        }
+        else
+        {
+          i++;
+        }
+      }
     }
     
   }
--- Anagram.Solution/Anagram/Program.cs
+++ Anagram.Solution/Anagram/Program.cs
@@ -7,12 +7,11 @@ namespace Anagram
   {
     public static void Main()
     {
-      
       Words words = new Words();
       Console.WriteLine("Enter words you want to check for Anagrams:");
-      string input = Console.ReadLine();
+      string input = Console.ReadLine().ToLower();
       words.ConvertToArray(input);
-
+      words.CheckForAnagrams();
     }
   }
 }
